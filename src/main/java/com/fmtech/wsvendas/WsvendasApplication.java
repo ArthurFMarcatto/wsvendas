d02be275f4e82f922583c983632df58512205578
Arthur Marcatto <arthurmarcatto@gmail.com>
2023-10-19 23:46:15 -0300

    Projeto Criado

---
+++ src/main/java/com/fmtech/wsvendas/WsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.fmtech.wsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsvendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmtech/wsvendas/WsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmtech.wsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
