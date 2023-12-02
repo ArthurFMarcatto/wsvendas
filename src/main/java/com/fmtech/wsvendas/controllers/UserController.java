80d5c6fb06977f14fd061db7449303d3e4da132c
Arthur Marcatto <arthurmarcatto@gmail.com>
2023-12-02 17:54:23 -0300

    Criados os métodos findAll e findById em UserService.

--- src/main/java/com/fmtech/wsvendas/controllers/UserController.java
+++ src/main/java/com/fmtech/wsvendas/controllers/UserController.java
@@ -1,20 +1,34 @@
 package com.fmtech.wsvendas.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fmtech.wsvendas.entities.User;
+import com.fmtech.wsvendas.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
 
 	@GetMapping
-	public ResponseEntity<User> findall() {
-		User u = new User(1L, "Arthur", "arthurmarcatto@gmail.com", "1699999", "123");
-
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> listUser = userService.findAll();
+		return ResponseEntity.ok().body(listUser);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
